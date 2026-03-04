0157fdd96fa3be9e1d62a3a27491cb4acdb39c7d
MiFuong <phuongchipi7749@gmail.com>
2026-03-04 15:53:04 +0700

    Cap nhat giao dien admin vao project -4/3/2026

---
+++ QuanLyQuanBiA/WinFormsApp1/GUI/AdminGUI/DoanhThuGUI/DoanhThuAdminForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WinFormsApp1.GUI.AdminGUI.DoanhThuGUI
+{
+    public partial class DoanhThuAdminForm : Form
+    {
+        public DoanhThuAdminForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
